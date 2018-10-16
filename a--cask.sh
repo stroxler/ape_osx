0617730525d0c444a0c0b774140050c810edbac1
Steven Troxler <steven.troxler@gmail.com>
2018-10-16 14:01:40 -0700

    Add sulpherpool colors to iterm2 setup

--- a--cask.sh
+++ a--cask.sh
@@ -20,11 +20,12 @@ brew cask install kitty   # terminal emulator: use for vim
 brew cask install docker
 
 # iterm, and a couple of color themes
-#   (With this you have solarized light/dark, nord, gruvbox)
+#   (With this you have solarized light/dark, nord, gruvbox, sulpherpool)
 brew cask install iterm2
 mkdir ~/iterm2-colors
 curl https://raw.githubusercontent.com/arcticicestudio/nord-iterm2/develop/src/xml/Nord.itermcolors > ~/iterm2-colors/Nord.itermcolors
 curl https://raw.githubusercontent.com/morhetz/gruvbox-contrib/master/iterm2/gruvbox-dark.itermcolors > ~/iterm2-colors/Gruvbox.itermcolors
+curl https://raw.githubusercontent.com/chriskempson/base16-iterm2/master/base16-ateliersulphurpool.dark.itermcolors > ~/iterm2-colors/B16-sulpherpool.itermcolors
 # ide and git gui devtools
 brew cask install intellij-idea-ce
 brew cask install sourcetree
