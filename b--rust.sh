9fa029a20e4bf6304aa0477f3c2ba4db5e01da6a
Steven Troxler <steven.troxler@gmail.com>
2018-10-23 11:55:33 -0700

    Add rustup install

---
+++ b--rust.sh
@@ -0,0 +1,7 @@
+# Install rustup / rustc / cargo
+#
+# I'm not 100% sure the differences between all of these,
+# but between them they give you something kind of like
+# stack, where you can manage different rust versions and
+# also the package dependencies of various projects
+curl https://sh.rustup.rs -sSf | sh
