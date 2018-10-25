bce17a801a323cac860073140a33b527b8d09676
Steven Troxler <steven.troxler@gmail.com>
2018-10-25 13:00:09 -0700

    Add more rust tooling

--- b--rust.sh
+++ b--rust.sh
@@ -5,3 +5,23 @@
 # stack, where you can manage different rust versions and
 # also the package dependencies of various projects
 curl https://sh.rustup.rs -sSf | sh
+
+# get the standard library as source code (needed for racer)
+rustup component add rust-src
+
+# add formatting
+rustup component add rustfmt-preveiw
+
+# add a more powerful linter
+rustup component add clippy-preview
+
+# add rust language server
+rustup component add rls-preview
+
+# and rust code analysis
+rustup component add rust-analysis
+
+# install racer for autocomplete
+rustup toolchain add nightly
+cargo +nightly install racer
+cargo +nightly install rustsym
