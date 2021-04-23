50ccdaef241796831cfcfd487573ce9521ba8de2
Steven Troxler <steven.troxler@gmail.com>
2021-04-22 23:19:50 -0400

    Change script name to demacs-script

--- d--doom-emacs.sh
+++ d--doom-emacs.sh
@@ -3,6 +3,6 @@ set -ex
 
 git clone https://github.com/hlissner/doom-emacs ~/doom-emacs.d
 
-# assumes you have the shim at stroxler/config/bin/doom-emacs
-yes | doom-emacs install
-yes | doom-emacs sync
+# assumes you have the shim at stroxler/config/bin/demacs/script
+yes | demacs-script install
+yes | demacs-script sync
