5f16519df7f62b2a27fa01fe45d0e30a74f1ffc6
Steven Troxler <steven.troxler@gmail.com>
2023-04-19 12:48:20 -0700

    Use actual doom location in doom install scriptlet

--- d--doom-emacs.sh
+++ d--doom-emacs.sh
@@ -4,5 +4,5 @@ set -ex
 git clone https://github.com/hlissner/doom-emacs ~/doom-emacs.d
 
 # assumes you have the shim at stroxler/config/bin/demacs/script
-yes | demacs-script install
-yes | demacs-script sync
+yes | ~/doom-emacs.d/bin/doom install
+yes | ~/doom-emacs.d/bin/doom sync
