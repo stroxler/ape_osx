a922725d39cc5d12bfda8d410a86d851bbab1d6c
Steven Troxler <steven.troxler@gmail.com>
2023-04-24 18:39:04 -0700

    Move doomacs install to `d--emacs-environments`
    It seems to be working now!

--- d--doom-emacs.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env sh
-set -ex
-
-git clone https://github.com/hlissner/doom-emacs ~/doom-emacs.d
-
-# assumes you have the shim at stroxler/config/bin/demacs/script
-yes | ~/doom-emacs.d/bin/doom install
-yes | ~/doom-emacs.d/bin/doom sync
--- d--emacs-environments.sh
+++ d--emacs-environments.sh
@@ -14,7 +14,23 @@ git clone https://github.com/syl20bnr/spacemacs ~/spacemacs-emacs.d
 #  remove .emacs.d when bootstrapping some alternative emacs setup,
 #  since the install instructions usually expect to install there
 #  and it lets us take chemacs2 out of the loop if there's an issue.
-ln -s ~/chemacs2-emacs.d ~/.emacs.d
+ln -s ~/chemacs2-emacs.d ~/.config/emacs
 
-# Note: my chemacs2 configuration is in dotconfar
+# make sure there's no legacy emacs
+rm -rf ~/.emacs.d
+
+
+# doom-emacs install. Note: I'm not actually sure where straight
+# puts the packages installed here! But it does seem to work.
+git clone https://github.com/hlissner/doom-emacs ~/doom-emacs.d
+echo "Installing doom packages. This will take a while."
+yes | ~/doom-emacs.d/bin/doom install
+# not really needed, but remember this has to be run when we
+# change dependencies in our doom config!
+yes | ~/doom-emacs.d/bin/doom sync
+
+
+# Notes
+# - my chemacs2 configuration is in dotconfar
+# - my runemacs-emacs.d config is also in dotconfar
 
